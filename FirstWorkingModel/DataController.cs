65909574c8ea432e643a7b4a4c8963ab3bb74b6f
Lewis Barnes <362490@edu.cwa.ac.uk>
2017-10-19 14:14:36 +0100

    Added controller for data

---
+++ FirstWorkingModel/DataController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstWorkingModel
+{
+    class DataController
+    {
+        private static DataController m_getInstance;
+        public static DataController getInstance()
+        {
+            if(m_getInstance == null)
+            {
+                m_getInstance = new DataController();
+            }
+            return m_getInstance;
+        }
+    }
+}
