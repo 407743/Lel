5fdf0c61e86f4e0caf63799050a7327ff943570c
Lewis Barnes <362490@edu.cwa.ac.uk>
2017-10-19 11:51:19 +0100

    added switch case for login

--- FirstWorkingModel/Login.cs
+++ FirstWorkingModel/Login.cs
@@ -19,7 +19,29 @@ namespace FirstWorkingModel
 
         private void btn_Login_Click(object sender, EventArgs e)
         {
-
+            string user = txt_Username.Text.ToLower();
+            string password = txt_Password.Text.ToLower();
+            if (password == "password123")
+            {
+                switch (user[0])
+                {
+                    case 'p':
+                        break;
+                    case 'r':
+                        break;
+                    case 'd':
+                        break;
+                    case 'n':
+                        break;
+                    default:
+                        MessageBox.Show("Username not recognized");
+                        break;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Password incorrect");
+            }
         }
     }
 }
